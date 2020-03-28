ccdb1df1c550f996605790ac6b3f7744d18eec6e
JustinCheung6 <justincheung6@gmail.com>
2020-03-27 23:03:30 -0700

    - Created Unity Project -Created PlayerMovement -Added jumping to PlayerMovement (no groundcheck yet) - Added groundcheck to test - Added first person camera rotation (Turned off until playermovement in local space is found out

---
+++ Evil AutoRunner/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Tooltip("How fast the player speeds up")]
+    [SerializeField] private float acceleration = 1f;
+
+    [SerializeField] private float maxSpeed = 1f;
+
+    [Tooltip("How fast the player slows down")]
+    [SerializeField] private float deceleration = 1f;
+
+    [Tooltip("Velocity that player is set to on jump")]
+    [SerializeField] private float jumpForce = 0f;
+
+    //Number of jumps player can perform
+    private int jump = 0;
+    //Number of jumps player gets when they go back onto the floor
+    private int maxJumps = 2;
+
+    //Object References
+    private Rigidbody rb = null;
+
+    private void Awake()
+    {
+        if(rb == null)
+            rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float[] speed = { rb.velocity.x, rb.velocity.z };
+        float[] input = { Input.GetAxis("Horizontal"), Input.GetAxis("Vertical") };
+        float jump = Input.GetButtonDown("Jump") ? jumpForce : rb.velocity.y;
+
+        input[1] = 0f;
+
+        for (int i = 0; i < 2; i++)
+        {
+            if(input[i] != 0)
+            {
+                if ((speed[i] + acceleration)  < maxSpeed * input[i])
+                    speed[i] += acceleration * input[i];
+                else
+                    speed[i] = maxSpeed * input[i];
+            }
+            else
+            {
+                if (speed[i] > deceleration)
+                    speed[i] -= deceleration;
+                else
+                    speed[i] = 0;
+            }
+        }
+
+        Vector3 x = transform.right * speed[0];
+        Vector3 z = transform.forward * speed[1];
+
+        rb.velocity = new Vector3(0f, jump, 0f) + x + z;
+    }
+
+    private bool GroundCheck()
+    {
+        LayerMask floorLayer = 1 << LayerMask.NameToLayer("Floor");
+
+        return Physics.Raycast(transform.position, Vector3.down, 0.55f, floorLayer);
+    }
+}
---
+++ Evil AutoRunner/Assets/Scripts/PlayerRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [Tooltip("Camera rotation speed. (0 for horizontal, 1 for vertical)")]
+    [SerializeField] private float rotateHorizontal = 0;
+    [SerializeField] private float rotateVertical = 0;
+
+    private float[] mouseRotation = new float[3];
+
+    //Object References
+    private Transform cameraTrans;
+
+    private void Awake()
+    {
+        cameraTrans = GetComponentInChildren<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float[] input = { Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"), 0 };
+        float[] speed = { rotateHorizontal, rotateVertical, 0f };
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(input[i] != 0)
+            {
+                mouseRotation[i] += speed[i] * input[i];
+            }
+        }
+
+        transform.eulerAngles = new Vector3(0f, mouseRotation[0], 0f);
+        cameraTrans.eulerAngles = new Vector3(mouseRotation[1], 0f, 0f);
+    }
+}
